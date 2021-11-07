fd07df899662a9724de498eab70f6a91047ab3c2
gitthubbann <photo2000photo@gmail.com>
2021-11-07 11:13:35 +0100

    final version

--- pezos/src/tools/WrongTagFromSocketException.java
+++
@@ -1,9 +0,0 @@
-package tools;
-@SuppressWarnings("serial")
-
-public class WrongTagFromSocketException extends Exception {
-	
-    public WrongTagFromSocketException(String message){
-        super(message);
-    }
-}
