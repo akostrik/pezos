44068ce4c77fccf9a542467c20bea69199b9c0b7
gitthubbann <92798527+gitthubbann@users.noreply.github.com>
2021-11-08 10:10:24 +0100

    Update Main.java

--- pezos/src/pezos/Main.java
+++ pezos/src/pezos/Main.java
@@ -1,5 +1,3 @@
-// level 1?
-// tag 3 level 1 la réponse est très très long
 package pezos;
 
 import java.io.IOException;
@@ -13,21 +11,26 @@ import org.bouncycastle.crypto.CryptoException;
 import org.bouncycastle.crypto.DataLengthException;
 
 public class Main {
-	public static void main(String[] args) throws DataLengthException, DecoderException, CryptoException, InvalidKeyException, SignatureException, NoSuchAlgorithmException, InvalidKeySpecException, InterruptedException {
+	public static void main(String[] args) throws DataLengthException, DecoderException, CryptoException, InvalidKeyException, SignatureException, NoSuchAlgorithmException, InvalidKeySpecException, InterruptedException, IOException {
 		final String hostname = "78.194.168.67";
 		final int    port     = 1337;
 		final String pk       = "b8b606dba2410e1f3c3486e0d548a3053ba3f907860fada6fab2835fb27b3f21"; // public
 		final String sk       = "1f06949f1278fcbc0590991180d5b567d240c0b0576d1d34cad66db49d4eea4a"; // secret
 		final int    secondesBetweenBroadcastes = 600;
-	
 		Connection connection = null;
+	
 		try {
 			connection = new Connection(hostname,port,pk,sk);
-			new LoopAuto(connection,pk,sk,secondesBetweenBroadcastes);
-			//new LoopREPL(connection, pk, sk);
+			//new LoopAuto(connection,pk,sk,secondesBetweenBroadcastes);
+			new LoopREPL(connection, pk, sk);
 		} catch (IOException e) {
-			System.out.println("IOException in main, problem with the socket or the internet connection");
-			return;
+		}
+		finally {
+			try {
+				connection.close();
+			}
+			catch (NullPointerException e) {
+			}
 		}
 	}
 }
