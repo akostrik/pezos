b05abf4dce6d561bcf30cbb9437e5b217110f336
gitthubbann <92798527+gitthubbann@users.noreply.github.com>
2021-11-08 10:12:17 +0100

    Update LoopREPL.java

--- pezos/src/pezos/LoopREPL.java
+++ pezos/src/pezos/LoopREPL.java
@@ -7,6 +7,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import org.apache.commons.codec.DecoderException;
 import org.bouncycastle.crypto.CryptoException;
@@ -21,7 +22,7 @@ public class LoopREPL {
 	String           sk;
 	Scanner          scanner = null;
 	
-	public LoopREPL(Connection connection, String pk, String sk) throws InvalidKeyException, DataLengthException, SignatureException, InvalidKeySpecException, NoSuchAlgorithmException, IOException, DecoderException, CryptoException {
+	public LoopREPL(Connection connection, String pk, String sk) throws InvalidKeyException, DataLengthException, SignatureException, InvalidKeySpecException, NoSuchAlgorithmException, DecoderException, CryptoException {
 		this.out     = connection.getOut();
 		this.in      = connection.getIn();
 		this.pk      = pk;
@@ -30,36 +31,28 @@ public class LoopREPL {
 
 		while(true) {
 			try {
-				oneIteration();
-			} catch (IOException e) {
-				System.out.println("IOException in LoopREPL, may be a problem with the socket or the internet connection");
-				connection.close();
-				return;
-			}
-			catch(NoSuchElementException e) {
-		            	return;
+				int tag = getTag();
+				if (tag == 1) {
+					Block lastBlock = Utils.getCurrentHead(this.out,this.in);
+					System.out.println("I verify errors of the block "+lastBlock);
+					lastBlock.verifyErrors(out,in,pk,sk);
+				} else {
+					int level = getLevel();
+					if (tag == 3) 
+						System.out.println("I jave got the block of level "+level+" :\n"+Utils.getBlock(level,this.out,this.in));
+					else if (tag == 5) 
+						System.out.println("I have got the list of operations of level "+level+" :\n"+Utils.getListOperations(level,this.out,this.in));
+					else if (tag == 7) 
+						System.out.println("I have got the state of level "+level+" :\n"+Utils.getState(level,this.out,this.in));
+				}
+			} catch (IOException | NoSuchElementException e) {
+				System.out.println("IOException or | NoSuchElementException, may be a problem with the socket or the internet connection");
+				break;
 			}
 		}
 	}
 	
-	public void oneIteration() throws IOException, DecoderException, InvalidKeyException, DataLengthException, SignatureException, InvalidKeySpecException, NoSuchAlgorithmException, CryptoException {
-		int tag = getTag();
-		int level = (tag==3 || tag==5 || tag==7) ? getLevel() : 0;
-		
-		if (tag == 1) {
-			Block lastBlock = Utils.getCurrentHead(this.out,this.in);
-			System.out.println("I verify errors of the block "+lastBlock);
-			lastBlock.verifyErrors(out,in,pk,sk);
-		} else if (tag == 3) {
-			System.out.println("I jave got the block of level "+level+" :\n"+Utils.getPredecessor(level,this.out,this.in));
-		} else if (tag == 5) {
-			System.out.println("I have got the list of operations of level "+level+" :\n"+Utils.getListOperations(level,this.out,this.in));
-		} else if (tag == 7) {
-			System.out.println("I have got the state of level "+level+" :\n"+Utils.getState(level,this.out,this.in));
-		}
-	}
-
-	public int getTag() {
+	public int getTag() throws NoSuchElementException {
 		try {
 			System.out.println("Give the tag (1, 3, 5 or 7): ");
 			return scanner.nextInt();
